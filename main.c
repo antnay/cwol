7562514da0352fd729cac71b187de2bc43f0d553
Anthony <antnay@uw.edu>
2025-12-16 13:12:36 -0800

    finish wol packet sending

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <ifaddrs.h>
 #include <netinet/in.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -8,7 +9,7 @@
 #include <sys/socket.h>
 
 #define WOL_SIZE 102
-#define MAC_SIZE 8 * 6
+#define MAC_SIZE 6
 
 // todo: db
 // todo: tui mode
@@ -48,11 +49,11 @@ int main(int argc, char *argv[]) {
   // TODO: make me generic
   inet_aton("10.0.69.255", &addr.sin_addr);
 
-  // int sendres = sendto(socketfd, packet, sizeof(packet), 0,
-  //                      (const struct sockaddr *)&addr, sizeof(addr));
-  // if (sendres == -1) {
-  //   perror("error sending packet");
-  // }
+  int sendres = sendto(socketfd, packet, WOL_SIZE, 0,
+                       (const struct sockaddr *)&addr, sizeof(addr));
+  if (sendres == -1) {
+    perror("error sending packet");
+  }
 
   return EXIT_SUCCESS;
 }
@@ -62,12 +63,12 @@ char *help() {}
 // FIXME: exit if unparsable
 inline uint8_t *makePacket(char *mac) {
   uint8_t newMac[MAC_SIZE];
-  uint8_t oct1 = (ctoi(mac[0]) << 4) ^ ctoi(mac[1]);
-  uint8_t oct2 = (ctoi(mac[3]) << 4) ^ ctoi(mac[4]);
-  uint8_t oct3 = (ctoi(mac[6]) << 4) ^ ctoi(mac[7]);
-  uint8_t oct4 = (ctoi(mac[9]) << 4) ^ ctoi(mac[10]);
-  uint8_t oct5 = (ctoi(mac[12]) << 4) ^ ctoi(mac[13]);
-  uint8_t oct6 = (ctoi(mac[15]) << 4) ^ ctoi(mac[16]);
+  uint8_t oct1 = (ctoi(mac[0]) << 4) | ctoi(mac[1]);
+  uint8_t oct2 = (ctoi(mac[3]) << 4) | ctoi(mac[4]);
+  uint8_t oct3 = (ctoi(mac[6]) << 4) | ctoi(mac[7]);
+  uint8_t oct4 = (ctoi(mac[9]) << 4) | ctoi(mac[10]);
+  uint8_t oct5 = (ctoi(mac[12]) << 4) | ctoi(mac[13]);
+  uint8_t oct6 = (ctoi(mac[15]) << 4) | ctoi(mac[16]);
 
   memcpy(newMac, &oct1, 1);
   memcpy(newMac + 1, &oct2, 1);
@@ -81,17 +82,16 @@ inline uint8_t *makePacket(char *mac) {
   };
 
   for (size_t i = 0; i < 16; i++) {
-    memcpy(packet + (i * 6) + 6, newMac, MAC_SIZE);
+    memcpy(packet + (i * MAC_SIZE) + 6, newMac, MAC_SIZE);
   }
 
-  // printf("\n");
-  // for (int i = 0; i < WOL_SIZE; i++) {
-  //   printf("%-3d ", packet[i]);
-  //   if (i % 6 == 0) {
-  //     printf("\n");
-  //   }
-  // }
-  // printf("\n");
+  for (int i = 0; i < WOL_SIZE; i++) {
+    printf("%-3x ", packet[i]);
+    if ((i + 1) % 6 == 0 && i != 0) {
+      printf("\n");
+    }
+  }
+  printf("\n");
 
   return packet;
 }
